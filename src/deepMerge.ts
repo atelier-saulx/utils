ac6ac36f371ba30fcff65283b3ee71a454227eb3
Jim de Beer <beerdejim@gmail.com>
2020-10-06 17:08:51 +0200

    make

--- src/deepMerge.ts
+++ src/deepMerge.ts
@@ -1,43 +1,36 @@
 const merge = (target: any, source: any) => {
-  if (
-    target &&
-    typeof target === 'object' &&
-    source &&
-    typeof source === 'object'
-  ) {
-    if (source.constructor === Array) {
-      for (let i = 0; i < source.length; i++) {
-        if (i in target) {
-          if (
-            target[i] &&
-            typeof target[i] === 'object' &&
-            source[i] &&
-            typeof source[i] === 'object'
-          ) {
-            merge(target[i], source[i])
-          } else {
-            target[i] = source[i]
-          }
+  if (source.constructor === Array) {
+    for (let i = 0; i < source.length; i++) {
+      if (i in target) {
+        if (
+          target[i] &&
+          typeof target[i] === 'object' &&
+          source[i] &&
+          typeof source[i] === 'object'
+        ) {
+          merge(target[i], source[i])
         } else {
           target[i] = source[i]
         }
+      } else {
+        target[i] = source[i]
       }
-    } else {
-      for (const i in source) {
-        if (i in target) {
-          if (
-            target[i] &&
-            typeof target[i] === 'object' &&
-            source[i] &&
-            typeof source[i] === 'object'
-          ) {
-            merge(target[i], source[i])
-          } else {
-            target[i] = source[i]
-          }
+    }
+  } else {
+    for (const i in source) {
+      if (i in target) {
+        if (
+          target[i] &&
+          typeof target[i] === 'object' &&
+          source[i] &&
+          typeof source[i] === 'object'
+        ) {
+          merge(target[i], source[i])
         } else {
           target[i] = source[i]
         }
+      } else {
+        target[i] = source[i]
       }
     }
   }
@@ -46,45 +39,55 @@ const merge = (target: any, source: any) => {
 export function deepMergeArrays(target: any, ...sources: any[]): any {
   if (!sources.length) return target
   if (sources.length === 1) {
-    merge(target, sources[0])
-    return target
+    const source = sources[0]
+    if (
+      target &&
+      typeof target === 'object' &&
+      source &&
+      typeof source === 'object'
+    ) {
+      merge(target, source)
+      return target
+    }
   }
   for (let i = 0; i < sources.length; i++) {
-    merge(target, sources[i])
+    const source = sources[i]
+    if (
+      target &&
+      typeof target === 'object' &&
+      source &&
+      typeof source === 'object'
+    ) {
+      merge(target, source)
+      return target
+    }
   }
   return target
 }
 
 const mergeExcludeArray = (target: any, source: any): any => {
-  if (
-    target &&
-    typeof target === 'object' &&
-    source &&
-    typeof source === 'object'
-  ) {
-    if (source.constructor === Array || target.constructor === Array) {
-      return source
-    } else {
-      for (const i in source) {
-        if (i in target) {
-          if (
-            target[i] &&
-            source[i] &&
-            typeof target[i] === 'object' &&
-            target[i].constructor !== Array &&
-            typeof source[i] === 'object' &&
-            source[i].constructor !== Array
-          ) {
-            const a = mergeExcludeArray(target[i], source[i])
-            if (a !== target[i]) {
-              target[i] = a
-            }
-          } else {
-            target[i] = source[i]
+  if (source.constructor === Array || target.constructor === Array) {
+    return source
+  } else {
+    for (const i in source) {
+      if (i in target) {
+        if (
+          target[i] &&
+          source[i] &&
+          typeof target[i] === 'object' &&
+          target[i].constructor !== Array &&
+          typeof source[i] === 'object' &&
+          source[i].constructor !== Array
+        ) {
+          const a = mergeExcludeArray(target[i], source[i])
+          if (a !== target[i]) {
+            target[i] = a
           }
         } else {
           target[i] = source[i]
         }
+      } else {
+        target[i] = source[i]
       }
     }
   }
@@ -94,12 +97,28 @@ const mergeExcludeArray = (target: any, source: any): any => {
 export function deepMerge(target: any, ...sources: any[]): any {
   if (!sources.length) return target
   if (sources.length === 1) {
-    return mergeExcludeArray(target, sources[0])
+    const source = sources[0]
+    if (
+      target &&
+      typeof target === 'object' &&
+      source &&
+      typeof source === 'object'
+    ) {
+      return mergeExcludeArray(target, source)
+    }
   }
   for (let i = 0; i < sources.length; i++) {
-    const a = mergeExcludeArray(target, sources[i])
-    if (a !== target) {
-      target = a
+    const source = sources[i]
+    if (
+      target &&
+      typeof target === 'object' &&
+      source &&
+      typeof source === 'object'
+    ) {
+      const a = mergeExcludeArray(target, source)
+      if (a !== target) {
+        target = a
+      }
     }
   }
   return target
