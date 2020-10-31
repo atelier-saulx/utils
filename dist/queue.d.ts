71b31ac86f164ab76d0dc7f9573de5aa7a593607
Jim de Beer <beerdejim@gmail.com>
2020-10-31 09:27:51 +0100

    make

--- dist/queue.d.ts
+++
@@ -1,4 +0,0 @@
-export default function promiseQueue(promiseFn: <T>(...args: any[]) => Promise<T>, opts?: {
-    concurrency?: number;
-    memoize?: (...args: any[]) => any;
-}): <T>(...args: any[]) => Promise<T>;
--- dist/stringToEnv.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (str) => {
-    return 'x';
-};
-//# sourceMappingURL=stringToEnv.js.map
