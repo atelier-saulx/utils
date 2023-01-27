cf34123a96c4285c2448c0297cf84eca79a54174
Jim de Beer <beerdejim@gmail.com>
2023-01-27 11:20:46 +0100

    remove old files

--- dist/serializeQuery.d.ts
+++
@@ -1,8 +0,0 @@
-declare type QueryValue = string | number | boolean;
-declare type QueryParams = {
-    [key: string]: QueryValue | QueryValue[] | {
-        [key: string]: any;
-    };
-};
-export declare const parseQuery: (query: string) => QueryParams | void;
-export {};
