afff0d47ef1984c03fb96d062f4a0327689729bb
Jim de Beer <beerdejim@gmail.com>
2023-03-26 06:08:16 +0200

    correct type in deepCopy

--- src/deepCopy.ts
+++ src/deepCopy.ts
@@ -1,13 +1,17 @@
-const deepCopy = (a: object): object => {
-    const r = a.constructor === Array ? [] : {}
-    for (let k in a) {
-      if (a[k] !== null && typeof a[k] === 'object') {
-        r[k] = deepCopy(a[k])
-      } else {
-        r[k] = a[k]
-      }
+const deepCopy = <
+  T extends { [key: string]: any | undefined } | (any | undefined)[]
+>(
+  a: T
+): T => {
+  const r: T = <T>(Array.isArray(a) ? [] : {})
+  for (const k in a) {
+    if (a[k] !== null && typeof a[k] === 'object') {
+      r[k] = deepCopy(a[k])
+    } else {
+      r[k] = a[k]
     }
-    return r
   }
+  return r
+}
 
-  export default deepCopy
+export default deepCopy
