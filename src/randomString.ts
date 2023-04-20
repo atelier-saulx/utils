63617e4209d53a14fb22f58f2bf392b84b062693
Nika <nika.tomadze@gmail.com>
2023-04-20 15:42:01 +0400

    Update randomString.ts
    safe null checks for ? notation

--- src/randomString.ts
+++ src/randomString.ts
@@ -13,10 +13,10 @@ export default function randomString(
     noNumbers?: boolean;
   }
 ) {
-  const noSpecials = opts?.noSpecials || false
-  const noLowerCase = opts?.noLowerCase || false
-  const noUpperCase = opts?.noUpperCase || false
-  const noNumbers = opts?.noNumbers || false
+  const noSpecials = opts && opts.noSpecials || false
+  const noLowerCase = opts && opts.noLowerCase || false
+  const noUpperCase = opts && opts.noUpperCase || false
+  const noNumbers = opts && opts.noNumbers || false
 
   const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz'
